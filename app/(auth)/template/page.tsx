1464419a00716f6029cbbc3203f83990e78bda60
Vaibhav <qvmail83@gmail.com>
2024-05-14 19:54:57 +0000

    trynow

--- app/(auth)/template/page.tsx
+++ app/(auth)/template/page.tsx
@@ -93,19 +93,20 @@ export default async function Template() {
                         alt={template?.product?.name} />
 
 
-                      <div className='flex justify-between gap-4'>
+                      <div className='p-6'>
 
-                      <Link className='flex gap-2' href={"/template/" + template.id}  target="_blank" rel="noopener noreferrer">
-                        <span className="text-start mt-2 mb-1 font-bold hover:text-orange-600">{template?.product?.name}</span>
-                        <span className="ml-2 m-auto mb-3"><svg width="12" height="11" viewBox="0 0 12 11" xmlns="http://www.w3.org/2000/svg"><g stroke="#52667A" stroke-width="1.25" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M4.10437012.76923H1.83333333C1.37309604.76923 1 1.11362635 1 1.53846077v7.69230769c0 .42483442.37309604.76923077.83333333.76923077h8.33333334c.46023729 0 .83333333-.34439635.83333333-.76923077V7.03051681M7.4205317.78000087h3.20254469c.21241721 0 .38461538.17219818.38461538.38461539V4.345704M10.5 1.5L6 5.84615385"></path></g></svg></span>
-                      </Link>
+                        <Link className='flex gap-2' href={"/template/" + template.id} target="_blank" rel="noopener noreferrer">
+                          <span className="text-start mt-2 mb-1 font-bold hover:text-orange-600">{template?.product?.name}</span>
+                          <span className="ml-2 m-auto mb-3"><svg width="12" height="11" viewBox="0 0 12 11" xmlns="http://www.w3.org/2000/svg"><g stroke="#52667A" stroke-width="1.25" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M4.10437012.76923H1.83333333C1.37309604.76923 1 1.11362635 1 1.53846077v7.69230769c0 .42483442.37309604.76923077.83333333.76923077h8.33333334c.46023729 0 .83333333-.34439635.83333333-.76923077V7.03051681M7.4205317.78000087h3.20254469c.21241721 0 .38461538.17219818.38461538.38461539V4.345704M10.5 1.5L6 5.84615385"></path></g></svg></span>
+                        </Link>
+
+                        <p className="text-sm text-gray-400 mt-2 mb-3">{template?.product?.description}</p>
 
                         <span className="bg-gray-100 border-gray-200 border-dashed border-2 p-1 px-4 rounded my-2">Helpdesk</span>
 
                       </div>
 
                     </div>
-                    <p className="text-sm text-gray-400 mt-2 mb-3">{template?.product?.description}</p>
 
 
 
