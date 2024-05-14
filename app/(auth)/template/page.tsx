83868407e38ad33ebf6ae79453116a33804c0ba4
Vaibhav <qvmail83@gmail.com>
2024-05-14 20:11:41 +0000

    trynow

--- app/(auth)/template/page.tsx
+++ app/(auth)/template/page.tsx
@@ -1,147 +1,134 @@
-import Link from "next/link";
-import Image from "next/image";
-import { _loadFromJson } from "@/app/utils/helper";
-import notionfooterImage from "@/public/images/freedesigner.png";
+import Link from 'next/link';
+import Image from 'next/image';
+import { _loadFromJson } from '@/app/utils/helper';
+import notionfooterImage from '@/public/images/freedesigner.png';
+
+
 
 export const metadata = {
-  title: "Notion Bear Templates",
-  description:
-    "Jumpstart your SaaS business with pre-built solutions from NotionBear and our community",
-};
+  title: 'Notion Bear Templates',
+  description: 'Jumpstart your SaaS business with pre-built solutions from NotionBear and our community',
+}
+
 
 export default async function Template() {
+
   const templates = await _loadFromJson();
+
+
 
   return (
     <section className="bg-gradient-to-b from-gray-100 to-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-18 md:pb-20">
+
           {/* Page header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1 mb-4">Find your Template</h1>
-            <p className="text-xl text-gray-600">
-              Jumpstart your SaaS business with pre-built solutions from
-              NotionBear and our community.
-            </p>
+            <p className="text-xl text-gray-600">Jumpstart your SaaS business with pre-built solutions from NotionBear and our community.</p>
 
             <div className="mt-6">
-              <div className="relative bg-gray-900 rounded py-6 px-4 md:py-8 md:px-12 shadow-2xl overflow-hidden">
+
+              <div className="relative bg-gray-900 rounded py-6 px-4 md:py-8 md:px-12 shadow-2xl overflow-hidden"
+              >
+
                 {/* Background illustration */}
-                <div className="absolute right-0 bottom-0 pointer-events-none hidden lg:block">
-                  <Image
-                    alt="Logo"
+                <div className="absolute right-0 bottom-0 pointer-events-none hidden lg:block" >
+
+
+
+                  <Image alt="Logo"
                     width={220}
                     className="block"
-                    src={notionfooterImage}
-                  />
+                    src={notionfooterImage} />
+
+
+
                 </div>
 
                 <div className="relative flex flex-col lg:flex-row justify-between items-center">
+
                   {/* CTA content */}
                   <div className="text-center lg:text-left lg:max-w-xl">
-                    <h3 className="h3 text-white mb-2">
-                      Get{" "}
-                      <b className="text-orange-600">Free Design Services</b>
-                    </h3>
+                    <h3 className="h3 text-white mb-2">Get <b className="text-orange-600">Free Design Services</b></h3>
 
                     {/* CTA form */}
                     <form className="w-full lg:w-auto">
                       <div>
-                        <a
-                          className="btn bg-orange-600 hover:bg-orange-700 shadow"
-                          href="https://buy.stripe.com/5kAeV0b6K27w8BG6os"
-                        >
-                          Sign Up
-                        </a>
+                        <a className="btn bg-orange-600 hover:bg-orange-700 shadow" href="https://buy.stripe.com/5kAeV0b6K27w8BG6os">Sign Up</a>
                       </div>
                       {/* Success message */}
                       {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}
-                      <p className="text-sm text-gray-400 mt-3">
-                        Subsribe to a monthly plan
-                      </p>
+                      <p className="text-sm text-gray-400 mt-3">Subsribe to a monthly plan</p>
                     </form>
                   </div>
+
                 </div>
+
               </div>
+
             </div>
+
           </div>
+
+
+
+
 
           <div className="max-w-3xl mx-auto aos-init aos-animate max-w-sm grid md:grid-cols-1 lg:grid-cols-2 items-start md:max-w-2xl lg:max-w-none gap-6 ">
             {templates.map((template: any, index: number) => (
-              <div
-                key={index}
-                className="relative flex flex-col items-center  bg-white rounded shadow-xl h-full border"
-              >
+
+
+
+
+              <div key={index} className="relative flex flex-col items-center  bg-white rounded shadow-xl h-full border">
                 <div>
                   <div>
                     <div className="items-start">
-                      <Image
-                        className="w-auto mx-auto border"
+                      <Image className="w-auto mx-auto border"
                         width={100}
                         height={100}
                         src={template?.product?.logo}
                         unoptimized
-                        alt={template?.product?.name}
-                      />
+                        alt={template?.product?.name} />
 
-                      <div className="p-6">
-                        <Link
-                          href={template?.ViewDemo?.link}
-                          className="flex gap-2"
-                        >
-                          <span className="text-start mt-2 mb-1 font-bold hover:text-orange-600">
-                            {template?.product?.name}
-                          </span>
-                          <span className="ml-2 m-auto mb-3">
-                            <svg
-                              width="12"
-                              height="11"
-                              viewBox="0 0 12 11"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <g
-                                stroke="#52667A"
-                                stroke-width="1.25"
-                                fill="none"
-                                fill-rule="evenodd"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                              >
-                                <path d="M4.10437012.76923H1.83333333C1.37309604.76923 1 1.11362635 1 1.53846077v7.69230769c0 .42483442.37309604.76923077.83333333.76923077h8.33333334c.46023729 0 .83333333-.34439635.83333333-.76923077V7.03051681M7.4205317.78000087h3.20254469c.21241721 0 .38461538.17219818.38461538.38461539V4.345704M10.5 1.5L6 5.84615385"></path>
-                              </g>
-                            </svg>
-                          </span>
+
+                      <div className='p-6'>
+
+                        <Link href={ template.id}  className='flex gap-2'>
+                          <span className="text-start mt-2 mb-1 font-bold hover:text-orange-600">{template?.product?.name}</span>
+                          <span className="ml-2 m-auto mb-3"><svg width="12" height="11" viewBox="0 0 12 11" xmlns="http://www.w3.org/2000/svg"><g stroke="#52667A" stroke-width="1.25" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M4.10437012.76923H1.83333333C1.37309604.76923 1 1.11362635 1 1.53846077v7.69230769c0 .42483442.37309604.76923077.83333333.76923077h8.33333334c.46023729 0 .83333333-.34439635.83333333-.76923077V7.03051681M7.4205317.78000087h3.20254469c.21241721 0 .38461538.17219818.38461538.38461539V4.345704M10.5 1.5L6 5.84615385"></path></g></svg></span>
                         </Link>
 
-                        <p className="text-sm text-gray-400 mt-2 mb-3">
-                          {template?.product?.description}
-                        </p>
+                        <p className="text-sm text-gray-400 mt-2 mb-3">{template?.product?.description}</p>
 
-                        <span className="bg-gray-100 border-gray-200 border-dashed border-2 p-1 px-4 rounded my-2">
-                          Helpdesk
-                        </span>
+                        <span className="bg-gray-100 border-gray-200 border-dashed border-2 p-1 px-4 rounded my-2">Helpdesk</span>
+
                       </div>
+
                     </div>
 
+
+
                     <div className="flex gap-4 justify-between">
+
                       <button className="bg-gray-900 border p-2 px-4 rounded my-2 text-white w-full mt-4 rounded text-center">
                         {template?.callToCopy?.text || "Duplicate"}
                       </button>
 
-                      <Link
-                        href={"/template/" + template.id}
-                        className="bg-gray-200 border p-2 px-4 rounded my-2 border border-gray-600 w-full mt-4 text-center rounded"
-                      >
+                      <Link href={"/template/" + template.id}  className="bg-gray-200 border p-2 px-4 rounded my-2 border border-gray-600 w-full mt-4 text-center rounded">
                         {template?.callToCopy?.text || "View Site"}
                       </Link>
                     </div>
+
                   </div>
                 </div>
               </div>
             ))}
+
           </div>
         </div>
       </div>
     </section>
-  );
+  )
 }
