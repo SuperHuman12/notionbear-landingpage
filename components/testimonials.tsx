39d8f30f778c055ac1e127ea0b9cf3dfe880c4cf
Vaibhav <qvmail83@gmail.com>
2024-05-14 19:19:44 +0000

    trynow

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -125,7 +125,7 @@ export default function Testimonials() {
             className="max-w-3xl mx-auto mt-8 aos-init aos-animate max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none"
             data-aos="zoom-y-out"
           >
-            <div className="relative flex flex-col items-center bg-white rounded shadow-xl h-full border">
+            <div className="relative flex flex-col items-center bg-white rounded-md shadow-xl h-full border">
               {/* Testimonial */}
               <div className="px-12 py-8 pt-12 mx-4 md:mx-0">
                 <div className="mb-4">
@@ -160,7 +160,7 @@ export default function Testimonials() {
               </div>
             </div>
 
-            <div className="relative flex flex-col items-center bg-white rounded shadow-xl h-full border">
+            <div className="relative flex flex-col items-center bg-white rounded-md shadow-xl h-full border">
               {/* Testimonial */}
               <div className="px-12 py-8 pt-12 mx-4 md:mx-0">
                 <div className="mb-4">
@@ -193,7 +193,7 @@ export default function Testimonials() {
               </div>
             </div>
 
-            <div className="relative flex flex-col items-center bg-white rounded shadow-xl h-full border">
+            <div className="relative flex flex-col items-center bg-white rounded-md shadow-xl h-full border">
               {/* Testimonial */}
               <div className="px-12 py-8 pt-12 mx-4 md:mx-0">
                 <div className="mb-4">
@@ -226,7 +226,7 @@ export default function Testimonials() {
               </div>
             </div>
 
-            <div className="relative flex flex-col items-center bg-white rounded shadow-xl h-full border">
+            <div className="relative flex flex-col items-center bg-white rounded-md shadow-xl h-full border">
               {/* Testimonial */}
               <div className="px-12 py-8 pt-12 mx-4 md:mx-0">
                 <div className="mb-4">
@@ -260,7 +260,7 @@ export default function Testimonials() {
               </div>
             </div>
 
-            <div className="relative flex flex-col items-center bg-white rounded shadow-xl h-full border">
+            <div className="relative flex flex-col items-center bg-white rounded-md shadow-xl h-full border">
               {/* Testimonial */}
               <div className="px-12 py-8 pt-12 mx-4 md:mx-0">
                 <div className="mb-4">
@@ -293,7 +293,7 @@ export default function Testimonials() {
               </div>
             </div>
 
-            <div className="relative flex flex-col items-center bg-white rounded shadow-xl h-full border">
+            <div className="relative flex flex-col items-center bg-white rounded-md shadow-xl h-full border">
               {/* Testimonial */}
               <div className="px-12 py-8 pt-12 mx-4 md:mx-0">
                 <div className="mb-4">
