ff5a3d8ef424adfedfc275ee2612e23a7983a35e
Vaibhav <qvmail83@gmail.com>
2024-05-15 22:51:07 +0000

    trynew gropdown

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -43,17 +43,121 @@ export default function Header() {
                   Create a..
                 </button>
                 {dropdownOpen && (
-                  <ul className="absolute bg-white shadow-lg py-2">
-                    <li>
-                      <Link href="/option1" className="block px-4 py-2 text-gray-600 hover:text-gray-900">Option 1</Link>
-                    </li>
-                    <li>
-                      <Link href="/option2" className="block px-4 py-2 text-gray-600 hover:text-gray-900">Option 2</Link>
-                    </li>
-                    <li>
-                      <Link href="/option3" className="block px-4 py-2 text-gray-600 hover:text-gray-900">Option 3</Link>
-                    </li>
-                  </ul>
+
+<div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+    <div className="relative grid gap-6 px-5 py-6 bg-white sm:gap-8 sm:p-8">
+        <a href="/help-center-software-powered-by-notion" className="flex items-start p-3 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6 text-purple-600">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+                ></path>
+            </svg>
+            <div className="ml-4 -mt-0.5">
+                <p className="text-base font-medium text-gray-900">Help Center</p>
+                <p className="mt-1 text-sm text-gray-500">
+                    Create your professional self-service help center with Notion and HelpKit
+                </p>
+            </div>
+        </a>
+        <a href="/documentation-software-powered-by-notion" className="flex items-start p-3 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6 text-purple-600">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+                ></path>
+            </svg>
+            <div className="ml-4 -mt-0.5">
+                <p className="text-base font-medium text-gray-900">Documentation</p>
+                <p className="mt-1 text-sm text-gray-500">
+                    Create your world-class product documentation with Notion and HelpKit
+                </p>
+            </div>
+        </a>
+        <a href="/helpdesk-web-widget-powered-by-notion" className="flex items-start p-3 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6 text-purple-600">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                ></path>
+            </svg>
+            <div className="ml-4 -mt-0.5">
+                <p className="text-base font-medium text-gray-900">Embeddable Widget</p>
+                <p className="mt-1 text-sm text-gray-500">
+                    Show your help articles to your customers right on your own website with our customizable widget
+                </p>
+            </div>
+        </a>
+        <a href="/protected-access-for-notion-pages" className="flex items-start p-3 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="flex-shrink-0 w-6 h-6 text-purple-600">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                ></path>
+            </svg>
+            <div className="ml-4 -mt-0.5">
+                <p className="text-base font-medium text-gray-900">Protected Access</p>
+                <p className="mt-1 text-sm text-gray-500">
+                    Protect and control access to your HelpKit sites for members only. Earn money with your content.
+                </p>
+            </div>
+        </a>
+        <a href="/helpkit-ai" className="flex items-start p-3 -m-3 transition duration-150 ease-in-out border rounded-lg border-purple-600/10 hover:bg-gray-50">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="url(#grad1)" className="flex-shrink-0 w-6 h-6 stroke-purple-600">
+                <defs>
+                    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                        <stop offset="0%"></stop>
+                        <stop offset="50%"></stop>
+                        <stop offset="100%"></stop>
+                    </linearGradient>
+                </defs>
+                <path
+                    stroke-linecap="round"
+                    stroke-width="2"
+                    stroke-linejoin="round"
+                    d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+                ></path>
+            </svg>
+            <div className="ml-4 -mt-0.5">
+                <p className="relative text-base font-medium text-gray-900">
+                    HelpKit AI
+                    <span className="relative ml-1 text-xs bottom-1 gradient-text-accent">Recommended</span>
+                </p>
+                <p className="mt-1 text-sm text-gray-500">
+                    AI powered chatbot and user insights add-on trained on your company's knowledge base
+                </p>
+            </div>
+        </a>
+    </div>
+    <div className="px-5 py-5 space-y-6 bg-gray-50 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+        <div className="flow-root font-medium text-gray-900">
+            <a href="https://support.helpkit.so/" target="_blank" className="flex items-center text-base font-medium text-gray-900 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 p-3 -m-3">
+                <img src="/_nuxt/img/helpsite_layout.99f86ce.png" width="30" className="shadow-md" /> <span className="ml-3 text-sm">Help Center Demo <span className="opacity-90"></span></span>
+            </a>
+        </div>
+        <div className="flow-root">
+            <a href="https://docs.helpkit.so/" target="_blank" className="flex items-center text-base font-medium text-gray-900 transition duration-150 ease-in-out rounded-md cursor-pointer hover:bg-gray-100 p-3 -m-3">
+                <img src="/_nuxt/img/docsite_layout.e98f21d.png" width="30" className="shadow-md" /> <span className="ml-3 text-sm"> Doc Site Demo <span className="opacity-90"></span></span>
+            </a>
+        </div>
+    </div>
+    <div className="flex items-center justify-start pt-2 pb-6 pl-8 bg-cool-gray-50">
+        <a href="/#productvideo" className="inline-flex items-center p-3 -m-3 text-base font-medium text-gray-900 transition duration-150 ease-in-out rounded-md cursor-pointer hover:bg-gray-100">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6 text-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"></path>
+            </svg>
+            <span className="ml-3">Watch our video instruction</span>
+        </a>
+    </div>
+</div>
+
                 )}
               </li>
               <li>
