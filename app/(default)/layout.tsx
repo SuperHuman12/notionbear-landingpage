a72897045dce6530f9303d30766b4a24cc65940f
Vaibhav <qvmail83@gmail.com>
2024-06-03 12:50:32 +0000

    check clarity now

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react'
 import PlausibleProvider from 'next-plausible'
+import Script from 'next/script'
 
 import AOS from 'aos'
 import 'aos/dist/aos.css'
@@ -14,7 +15,7 @@ export default function DefaultLayout({
 }: {
   children: React.ReactNode
 }) {
-  useEffect(() => {658
+  useEffect(() => {
     AOS.init({
       once: true,
       disable: 'phone',
@@ -29,6 +30,15 @@ export default function DefaultLayout({
         {children}
       </main>
       <Footer />
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+        `}
+      </Script>
     </PlausibleProvider>
   )
 }
